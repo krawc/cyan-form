75e41a58fc4dfa28df60b300956f517b60231621
Konrad <konrad@MacBook-Pro-3.home>
2020-09-16 21:49:56 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Multistep from 'react-multistep';
 
 function App() {
+
+
+  const steps = [
+    {name: 'StepOne', component: <p>StepOne</p>},
+    {name: 'StepTwo', component: <p>StepTwo</p>},
+    {name: 'StepThree', component: <p>StepThree</p>},
+    {name: 'StepFour', component: <p>StepFour</p>}
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Multistep showNavigation={true} steps={steps}/>
     </div>
   );
 }
